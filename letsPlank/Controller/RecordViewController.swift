2d401b8bac233eb171f7795858a99e3a8b3ec587
Rin <rin.t.4921@gmail.com>
2021-01-13 22:07:21 +0900

    recordViewUI変更、名言収集完了

--- letsPlank/Controller/RecordViewController.swift
+++ letsPlank/Controller/RecordViewController.swift
@@ -10,11 +10,15 @@ import FSCalendar
 
 class RecordViewController: UIViewController {
     @IBOutlet weak var calender: FSCalendar!
-    @IBOutlet weak var subView: UIView!
-    @IBOutlet weak var stackView: UIStackView!
+    
+    @IBOutlet weak var iamgeView: UIImageView!
     @IBOutlet weak var continuousDaysLabel: UILabel!
-    @IBOutlet weak var totalDaysLabel: UILabel!
-    @IBOutlet weak var totalSecLabel: UILabel!
+    @IBOutlet weak var quoteView: UIView!
+    @IBOutlet weak var quoteTextView: UITextView!
+    
+    var topSafeAreaHeight: CGFloat = 0
+    var bottomSafeAreaHeight: CGFloat = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,23 +26,33 @@ class RecordViewController: UIViewController {
         self.navigationItem.title = "Record"
         self.calender.dataSource = self
         self.calender.delegate = self
-        setViews()
         
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        topSafeAreaHeight = self.view.safeAreaInsets.top
+        setViews()
     }
     
     
     
     func setViews() {
-        
-        let navigationBarHeight = CGFloat((self.navigationController?.navigationBar.frame.size.height)!)
-        calender.frame.size.height = self.view.frame.height / 3
-        calender.frame.size.width = self.view.frame.width
-        calender.layer.position = CGPoint(x: self.view.frame.width/2, y: navigationBarHeight*2 + self.view.frame.height/6 + 5)
+        let width = self.view.frame.width
+        let height = self.view.frame.height
+        calender.frame.size.height = height/3
+        calender.frame.size.width = width
+        calender.layer.position = CGPoint(x: width/2, y: topSafeAreaHeight + height/6)
         calender.appearance.headerTitleColor = .orange
         calender.appearance.todayColor = .systemTeal
         calender.appearance.weekdayTextColor = .orange
         
-        subView.layer.position = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height*2/3)
+        quoteView.frame.size.height = height/3
+        quoteView.frame.size.width = self.view.frame.width
+        quoteView.layer.position = CGPoint(x: width/2, y: height*3/4)
+        quoteTextView.frame.size.width = width*2/3 - 10
+        
     }
     
 }
@@ -48,10 +62,10 @@ extension RecordViewController: FSCalendarDataSource, FSCalendarDelegate {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy/M/d"
         let da = formatter.string(from: date)
-       
         
         print(da)
     }
     
     
 }
+
