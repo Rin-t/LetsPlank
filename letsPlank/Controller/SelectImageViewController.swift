ccf4f9995e5bb10100671a782394aeb689026a21
Rin <rin.t.4921@gmail.com>
2021-01-15 23:20:45 +0900

    recordImageの設定

--- letsPlank/Controller/SelectImageViewController.swift
+++ letsPlank/Controller/SelectImageViewController.swift
@@ -9,17 +9,22 @@ import UIKit
 
 class SelectImageViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageButton: UIButton!
     @IBOutlet weak var backBarButton: UIBarButtonItem!
     @IBOutlet weak var saveButton: UIButton!
     var selectedDay: String?
+    var topSafeAreaHeight: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
         navigationItem.title = selectedDay
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        topSafeAreaHeight = self.view.safeAreaInsets.top
+        setViews()
     }
     
     func setViews() {
@@ -27,17 +32,51 @@ class SelectImageViewController: UIViewController {
         let height = self.view.frame.height
         
         //imageviewのレイアウト
-        imageView.frame.size.width = width
-        imageView.frame.size.height = height+2/3
-        imageView.layer.position = CGPoint(x: width/2, y: 0)
+        imageButton.frame.size.width = width
+        imageButton.frame.size.height = height/2
+        imageButton.layer.position = CGPoint(x: width/2, y: topSafeAreaHeight + height/4)
+        imageButton.layer.borderWidth = 0.5
         
         saveButton.frame.size.width = 70
-        saveButton.frame.size.width = 90
+        saveButton.frame.size.width = 190
+        saveButton.layer.position = CGPoint(x: width/2, y: height*4/5)
     }
     
     @IBAction func tappedBackBarButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
-        print("tapped")
     }
     
+    @IBAction func tappedSaveButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tappedImageButton(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+}
+
+extension SelectImageViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editImage = info[.editedImage] as? UIImage {
+            imageButton.setImage(editImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            imageButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        imageButton.setTitle("", for:  .normal)
+        imageButton.imageView?.contentMode = .scaleAspectFit
+        imageButton.contentHorizontalAlignment = .fill
+        imageButton.contentVerticalAlignment = .fill
+        
+        imageButton.clipsToBounds = true
+        
+        
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
